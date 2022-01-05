83fd5a78f39fa968bf0806e2cdada1b3fd67fe53
RohitRS30 <96464353+RohitRS30@users.noreply.github.com>
2022-01-05 12:33:15 +0530

    Create variable.py

---
+++ day 1/variable.py
@@ -0,0 +1,17 @@
+# first program is add 2 number print output
+
+a =  int (input ("enter the first num")) 
+b = int (input ("enter the second num"))
+print("sum=",a+b)
+
+# second programm add two string
+
+x =  str (input ("enter the first name")) 
+y = str (input ("enter the second name"))
+print("your name=",x+y)
+
+#thrid add two flat
+
+r =  float(input ("enter the first num")) 
+s = float (input ("enter the second num"))
+print("total=",r+s)
