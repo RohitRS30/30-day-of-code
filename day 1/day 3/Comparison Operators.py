e55b59a3ed40a54e8b522edcaebb0dd081d6d494
RohitRS30 <96464353+RohitRS30@users.noreply.github.com>
2022-01-05 23:58:10 +0530

    day 3
    day 3 is good

---
+++ day 1/day 3/Comparison Operators.py
@@ -0,0 +1,37 @@
+#second task is Comparison Operators 
+#(a) == equal is show tue /false
+
+a= int (input("enter the number is ="))
+b= int (input("enter the number is ="))
+print ("number is equal =", a==b)
+
+# (b) !=	Not equal
+
+x= int (input("enter the number is ="))
+y= int (input("enter the number is ="))
+print ("number is not equal =", x!=y)
+
+# (c) >	Greater than
+
+p= int (input("enter the number is ="))
+r= int (input("enter the number is ="))
+print ("number is Greater than = " , p>r)
+
+# (d) <	Less than
+
+o= int (input("enter the number is ="))
+t= int (input("enter the number is ="))
+print ("number is less than = " , o<t)
+
+# (e) >=	Greater than or equal to
+
+i= int (input("enter the number is ="))
+j= int (input("enter the number is ="))
+print ("number is 	Greater than or equal to = " , i>=j)
+
+# (f) <=	Less than or equal to
+
+w= int (input("enter the number is ="))
+z= int (input("enter the number is ="))
+print ("number is Less than or equal to	=" , w<=z)
+
---
+++ day 1/day 3/arithmatic oprator.py
@@ -0,0 +1,18 @@
+#first task is arithmatic operater
+# (A) % modulus
+
+a= int (input("enter the number is ="))
+b= int (input("enter the number is ="))
+print ("ans =", a%b)
+
+# (B) ** Expand
+
+x= int (input("enter the number is ="))
+y= int (input("enter the number is ="))
+print ("ans =", x**y)
+
+# (C) // No decimal
+
+p= int (input("enter the number is ="))
+r= int (input("enter the number is ="))
+print ("ans =", p//r)
