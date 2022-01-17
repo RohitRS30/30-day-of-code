b7b178f0706e1c502f376cdc0c5752a867c7fcb1
RohitRS30 <96464353+RohitRS30@users.noreply.github.com>
2022-01-17 15:00:21 +0530

    Create reverse order .py

---
+++ day9/reverse order .py
@@ -0,0 +1,11 @@
+#task 1 print 100 in reverse order
+list1 = [10, 20, 30, 40,50,60,70,80,90,100]
+for num in reversed(list1):
+    print(num)
+
+#taSK 2 PRINT prime number
+even = [1,2,4,6,9,10,12,14,16,18,20,22,24,26,28,30]
+prime = [i + 1 for i in even ]
+print(prime)
+
+#task 3 series upto n terms
